e9a5363594c8664e6996a8c32e126c92889fdf70
Victor Alencar <munternl13@gmail.com>
2022-09-30 19:00:20 -0300

    feat: ensure MathAdapter resolver

--- src/infra/math-adapter/math.ts
+++ src/infra/math-adapter/math.ts
@@ -1,20 +1,41 @@
-import { Math } from '../../data/protocols/math'
-import { compile, EvalFunction } from 'mathjs'
+import { Math, Table } from '../../data/protocols/math'
+import { compile, abs } from 'mathjs'
 
 export class MathAdapter implements Math {
-  private f: EvalFunction
-
   async calculate (params: Math.Params): Promise<Math.Result> {
-    const { func } = params
-    this.f = compile(func)
-    return await new Promise(resolve => resolve({
-      iterations: 0,
-      root: 0,
-      table: []
-    }))
-  }
-
-  getF (): EvalFunction {
-    return this.f
+    const { func, interval, maxIterations, precision } = params
+    const f = compile(func)
+    let a = interval[0]
+    let fxa = f.evaluate({ x: a })
+    let b = interval[1]
+    let fxb = f.evaluate({ x: b })
+    let c = (a + b) / 2
+    let i = 0
+    const table: Table[] = []
+    while (i < maxIterations && abs(fxb - fxa) > precision) {
+      const fx = f.evaluate({ x: c })
+      if (fx * fxb < 0) {
+        a = c
+        fxa = fx
+      } else {
+        b = c
+        fxb = fx
+      }
+      c = (a + b) / 2
+      i++
+      table.push({
+        a,
+        fxa,
+        b,
+        fxb,
+        c,
+        fxc: f.evaluate({ x: c })
+      })
+    }
+    return {
+      root: c,
+      iterations: i,
+      table
+    }
   }
 }
