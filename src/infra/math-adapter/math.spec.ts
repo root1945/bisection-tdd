01a4b6e21b6480e2eb406bdf82b98f1b6aec9e99
Victor Alencar <munternl13@gmail.com>
2022-09-30 18:43:04 -0300

    test: ensure MathAdapter save in the variable if the compile return is success

--- src/infra/math-adapter/math.spec.ts
+++ src/infra/math-adapter/math.spec.ts
@@ -37,4 +37,16 @@ describe('MathAdapter', () => {
     })
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should save in the variable if the compile return is success', async () => {
+    const sut = makeSut()
+    const compileSpy = jest.spyOn(mathjs, 'compile')
+    await sut.calculate({
+      func: 'e^x + x/2',
+      interval: [-1, 0],
+      precision: 0.00001,
+      maxIterations: 100
+    })
+    expect(sut.getF()).toBe(compileSpy.mock.results[0].value)
+  })
 })
