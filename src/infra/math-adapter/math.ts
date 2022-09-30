c6c03a010f52a50a772e251990c4bc297a7ec33c
Victor Alencar <munternl13@gmail.com>
2022-09-30 18:42:53 -0300

    feat: ensure MathAdapter save in the variable if the compile return is success

--- src/infra/math-adapter/math.ts
+++ src/infra/math-adapter/math.ts
@@ -1,14 +1,20 @@
 import { Math } from '../../data/protocols/math'
-import { compile } from 'mathjs'
+import { compile, EvalFunction } from 'mathjs'
 
 export class MathAdapter implements Math {
+  private f: EvalFunction
+
   async calculate (params: Math.Params): Promise<Math.Result> {
     const { func } = params
-    compile(func)
+    this.f = compile(func)
     return await new Promise(resolve => resolve({
       iterations: 0,
       root: 0,
       table: []
     }))
+  }
+
+  getF (): EvalFunction {
+    return this.f
   }
 }
