e231f1f896f7e04a1e2770f883b71026bc166d57
Victor Alencar <munternl13@gmail.com>
2022-09-30 19:13:36 -0300

    feat: Dichotomy factory

---
+++ src/main/app.ts
@@ -0,0 +1,14 @@
+import dichotomyFactory from './factories/dichotomy'
+
+const dichotomy = dichotomyFactory()
+
+dichotomy.calculate({
+  func: 'x^2 - 2',
+  interval: [0, 2],
+  maxIterations: 100,
+  precision: 0.0001
+}).then(success => {
+  console.log(success)
+}).catch(error => {
+  console.log(error)
+})
---
+++ src/main/factories/dichotomy.ts
@@ -0,0 +1,13 @@
+import { MathCalculation } from '../../data/features/math-calculation'
+import { MathAdapter } from '../../infra/math-adapter/math'
+import { Dichotomy } from '../../presentation/dichotomy'
+import { ExcelAdapter } from '../../infra/excel-adapter/excel'
+
+const dichotomyFactory = (): Dichotomy => {
+  const math = new MathAdapter()
+  const excel = new ExcelAdapter()
+  const calculation = new MathCalculation(math, excel)
+  return new Dichotomy(calculation)
+}
+
+export default dichotomyFactory
