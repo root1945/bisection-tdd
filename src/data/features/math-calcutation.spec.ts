fb5973506e0e19ac7c9181508a965804abf72458
Victor Alencar <munternl13@gmail.com>
2022-09-30 14:51:12 -0300

    test: ensure MathCalculation call GenerateExcel with correct values

--- src/data/features/math-calcutation.spec.ts
+++ src/data/features/math-calcutation.spec.ts
@@ -1,9 +1,20 @@
 import { Math } from '../protocols/math'
 import { MathCalculation } from './math-calculation'
+import { GenerateExcel } from '../protocols/generate-excel'
 
 interface SutTypes {
   sut: MathCalculation
   mathStub: Math
+  generateExcelStub: GenerateExcel
+}
+
+const makeGenerateExcelStub = (): GenerateExcel => {
+  class GenerateExcelStub implements GenerateExcel {
+    async generate (params: GenerateExcel.Params): Promise<GenerateExcel.Result> {
+      return await new Promise(resolve => resolve({ generatedExcel: 'success' }))
+    }
+  }
+  return new GenerateExcelStub()
 }
 
 const makeMathStub = (): Math => {
@@ -25,10 +36,12 @@ const makeMathStub = (): Math => {
 
 const makeSut = (): SutTypes => {
   const mathStub = makeMathStub()
-  const sut = new MathCalculation(mathStub)
+  const generateExcelStub = makeGenerateExcelStub()
+  const sut = new MathCalculation(mathStub, generateExcelStub)
   return {
     sut,
-    mathStub
+    mathStub,
+    generateExcelStub
   }
 }
 
@@ -57,5 +70,22 @@ describe('MathCalculation', () => {
     }
     const promise = sut.calculate(params)
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call GenerateExcel with correct values', async () => {
+    const { sut, generateExcelStub } = makeSut()
+    const generateExcelSpy = jest.spyOn(generateExcelStub, 'generate')
+    const params = {
+      func: 'e^x + x/2',
+      interval: [-1, 0],
+      precision: 0.00001,
+      maxIterations: 100
+    }
+    await sut.calculate(params)
+    expect(generateExcelSpy).toHaveBeenCalledWith({
+      table: [
+        { a: 1, fxa: 1, b: 1, fxb: 1, c: 1, fxc: 1 }
+      ]
+    })
   })
 })
